928635ceb0ceb3f8e5d6d70190a79f5fe6fc22ac
DHANANJAY130 <dhananjayim81@gmail.com>
2020-09-25 13:13:36 +0530

    Added raw mode input with ECHO feature on
    In this current state of editor the program will exit whenever user
    inputs 'q' character

--- dcode.cpp
+++ dcode.cpp
@@ -2,12 +2,32 @@
 #include <iostream>
 #include <termios.h>
 
+void enableRawMode() {
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    //raw.c_lflag &= ~(ECHO); // turning off ECHO in raw mode
+
+    raw.c_lflag &= (ECHO);
+
+    // turning on echo in raw mode
+    // ECHO feature causes each key you type to be printed
+    // to the terminal
+    // In raw mode you can't see what you're typing when ECHO is off
+    // but it is taking the input nonetheless
+    // just like you enter your password
+    // when you switch into sudo mode in linux terminal
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main()
 {
+    enableRawMode();
     char c;
     // int count = 1;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (read(STDIN_FILENO, &c, 1) == 1 && c !='x');
     // while (read(STDIN_FILENO, &c, 1) == 1 && count++);
     // std::cout << "\n" << count << " " << c << "\n";
     return 0;
