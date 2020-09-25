f37ca4b168bcc9758249a2c3d85ef2695f482892
DHANANJAY130 <dhananjayim81@gmail.com>
2020-09-25 11:30:35 +0530

    added code for taking input from user in canonical mode
    - Canonical Mode
        - In this mode user keyboard input will only be send to your
          program when you hit enter.
        - This mode is useful for a programmer when they want to give some
          commands but not so much for text editors.

--- dcode.cpp
+++ dcode.cpp
@@ -1,5 +1,15 @@
+#include <unistd.h>
+#include <iostream>
+#include <termios.h>
+
+
 int main()
 {
+    char c;
+    // int count = 1;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    // while (read(STDIN_FILENO, &c, 1) == 1 && count++);
+    // std::cout << "\n" << count << " " << c << "\n";
     return 0;
 }
 
